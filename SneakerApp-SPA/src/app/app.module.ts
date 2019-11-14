a03962bafab8fea302bb540e676c8412122495cc
scottfireoved <scottfireoved@gmail.com>
2019-11-13 18:58:45 -0800

    added alertify alerts to application.

--- SneakerApp-SPA/src/app/app.module.ts
+++ SneakerApp-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { AlertifyService } from './_services/alertify.service';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 
@@ -23,7 +24,8 @@ import { HomeComponent } from './home/home.component';
       FormsModule
    ],
    providers: [
-      AuthService
+      AuthService,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
--- SneakerApp-SPA/src/app/register/register.component.ts
+++ SneakerApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +11,18 @@ export class RegisterComponent implements OnInit {
   // @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
 
-  model:any = {};
+  model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration Successful');
+      this.alertify.success('Registration Successful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
