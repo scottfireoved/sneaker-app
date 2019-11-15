23d3762a43c918a0f32bd3e7baa053f387aaeef3
scottfireoved <scottfireoved@gmail.com>
2019-11-14 21:30:21 -0800

    extended user class model. updated db migration.

--- SneakerApp.API/Models/User.cs
+++ SneakerApp.API/Models/User.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace SneakerApp.API.Models
 {
     public class User
@@ -10,5 +12,24 @@ namespace SneakerApp.API.Models
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
     }
 }
