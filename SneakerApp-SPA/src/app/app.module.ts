83ec7b294cde376d3a2996ea1116bb63c45ca48a
scottfireoved <scottfireoved@gmail.com>
2019-11-14 18:36:48 -0800

    added jwt helper service, added bootstrap theme styling and ngx boostrap. cleaned up navigation bar.

--- SneakerApp-SPA/src/app/app.module.ts
+++ SneakerApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { HomeComponent } from './home/home.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
--- SneakerApp-SPA/src/app/nav/nav.component.ts
+++ SneakerApp-SPA/src/app/nav/nav.component.ts
@@ -24,8 +24,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token; //return true or false base on if its empty or not
+    return this.authService.loggedIn();
   }
 
   logout() {
